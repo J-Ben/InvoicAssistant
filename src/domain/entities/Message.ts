16588eaf5e238c889655fd5ea3503bb50091c13c
Ben-J MK <benjaminmk9@gmail.com>
2026-01-16 16:00:33 +0100

    🐞 fix: use conventional breakline

--- src/domain/entities/Message.ts
+++ src/domain/entities/Message.ts
@@ -1,6 +1,6 @@
 export interface Message {
- id : string,
- text : string, 
- sender : 'user' | 'assistant',
- timestamp : Date
+  id: string;        // point-virgule
+  text: string;
+  sender: 'user' | 'assistant';
+  timestamp: Date;
 }
